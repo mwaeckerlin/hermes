b9c8026fdd3da0b13c4be46a36673d72aa4c7167
Marc Wäckerlin <4056726+mwaeckerlin@users.noreply.github.com>
2026-04-28 00:24:16 +0200

    added mcp-ithub

--- files/sandbox-entrypoint.sh
+++ files/sandbox-entrypoint.sh
@@ -27,6 +27,10 @@ if [ -n "${DOCKER_HOST:-}" ]; then
   echo "==== Enabling Docker Host ===="
   echo "DOCKER_HOST=${DOCKER_HOST}" >> /etc/environment
 fi
+if [ -n "${MCP_GITHUB_URL:-}" ]; then
+  echo "==== Setting MCP GitHub URL ===="
+  echo "MCP_GITHUB_URL=${MCP_GITHUB_URL}" >> /etc/environment
+fi
 chown -R "${RUN_USER}:${RUN_GROUP}" "${RUN_HOME}"
 chmod 750 "${RUN_HOME}"
 chmod 700 "${RUN_HOME}/.ssh"
